443ca8bd6c51e588d8e94dddc43eae4bc0ad7eaf
scottkelly36 <scottkelly36@gmail.com>
2022-07-07 06:07:09 +0100

    finished queries

--- app.js
+++ app.js
@@ -1,54 +1,45 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const {
-    getCategories,
-} = require('./controllers/categories.controllers');
+const { getCategories } = require("./controllers/categories.controllers");
 
 //reviews controllers
 const {
-    getReviewById,
-    patchReviewVotes,
-    getReviewComments
-} = require('./controllers/reviews.controller');
+  getReviewById,
+  patchReviewVotes,
+  getReviewComments,
+  getReviews,
+} = require("./controllers/reviews.controller");
 
 //errors
-const {
-    getUsers
-} = require('./controllers/users.controllers');
+const { getUsers } = require("./controllers/users.controllers");
 
 const {
-    handleServerErrors,
-    handleWrongEndpoint,
-    handleNotFound,
-    handleCustom,
-
-} = require('./middleware/errors.middleware');
-
+  handleServerErrors,
+  handleWrongEndpoint,
+  handleNotFound,
+  handleCustom,
+} = require("./middleware/errors.middleware");
 
 app.use(express.json());
 
+app.get("/api/categories", getCategories);
 
-app.get('/api/categories', getCategories);
+app.get("/api/reviews/:review_id", getReviewById);
+app.patch("/api/reviews/:review_id", patchReviewVotes);
+app.get("/api/reviews/:review_id/comments", getReviewComments);
+app.get("/api/reviews", getReviews);
 
-app.get('/api/reviews/:review_id', getReviewById);
-app.patch('/api/reviews/:review_id', patchReviewVotes);
-app.get('/api/reviews/:review_id/comments', getReviewComments);
+app.get("/api/users", getUsers);
 
-app.get('/api/users', getUsers);
-
-
-app.use('*', (req, res) => {
-    res.status(404).send({
-        msg: "Sorry we cant find that end point"
-    })
+app.use("*", (req, res) => {
+  res.status(404).send({
+    msg: "Sorry we cant find that end point",
+  });
 });
 
-
 app.use(handleNotFound);
-app.use(handleCustom)
+app.use(handleCustom);
 app.use(handleServerErrors);
-
-
 
 module.exports = app;
--- models/review.model.js
+++ models/review.model.js
@@ -1,58 +1,103 @@
 const DB = require("../db/connection");
 
-
 exports.selectReviewById = (id) => {
-
-    return DB.query(`
+  return DB.query(
+    `
     SELECT reviews.*, COUNT(comments.*) AS comment_count
     FROM reviews
     LEFT JOIN comments ON
     reviews.review_id=comments.review_id
     WHERE reviews.review_id = $1
     GROUP BY reviews.review_id;
-    `, [id])
-        .then((result) => {
-
-            if (result.rows.length === 0) {
-                return Promise.reject({
-                    status: 404,
-                    msg: "Sorry Review cant be found"
-                });
-            } else {
-
-                return result.rows[0];
-            }
-
-        })
-}
+    `,
+    [id]
+  ).then((result) => {
+    if (result.rows.length === 0) {
+      return Promise.reject({
+        status: 404,
+        msg: "Sorry Review cant be found",
+      });
+    } else {
+      return result.rows[0];
+    }
+  });
+};
 
 exports.updateReviewVotes = (id, body) => {
-    const {
-        inc_votes
-    } = body
-    if (typeof inc_votes !== "number") {
-        return Promise.reject({
-            status: 400,
-            msg: 'Sorry incorrect datatype for votes'
-        })
+  const { inc_votes } = body;
+  if (typeof inc_votes !== "number") {
+    return Promise.reject({
+      status: 400,
+      msg: "Sorry incorrect datatype for votes",
+    });
+  }
+
+  return DB.query(
+    `UPDATE reviews SET votes = votes+$1 WHERE review_id = $2 RETURNING *`,
+    [inc_votes, id]
+  ).then((result) => {
+    if (result.rows.length === 0) {
+      return Promise.reject({
+        status: 404,
+        msg: "Sorry Review cant be found",
+      });
+    } else {
+      return result.rows[0];
     }
-
-
-    return DB.query(`UPDATE reviews SET votes = votes+$1 WHERE review_id = $2 RETURNING *`, [inc_votes, id]).then((result) => {
-        if (result.rows.length === 0) {
-            return Promise.reject({
-                status: 404,
-                msg: "Sorry Review cant be found"
-            });
-        } else {
-            return result.rows[0];
-        }
-
-    })
-}
+  });
+};
 
 exports.selectReviewComments = (id) => {
-    return DB.query(`SELECT * FROM comments WHERE review_id =$1`, [id]).then((result) => {
-        return result.rows;
-    })
-}
+  return DB.query(`SELECT * FROM comments WHERE review_id =$1`, [id]).then(
+    (result) => {
+      return result.rows;
+    }
+  );
+};
+
+exports.selectReviews = (sort_by = "created_at", order = "ASC", category) => {
+  const queryValues = [];
+  const validSortBy = [
+    "created_at",
+    "review_id",
+    "title",
+    "designer",
+    "owner",
+    "votes",
+    "category",
+  ];
+  const validOrder = ["ASC", "asc", "DESC", "desc"];
+
+  if (!validSortBy.includes(sort_by)) {
+    return Promise.reject({
+      status: 400,
+      msg: "Invalid sort_by",
+    });
+  }
+
+  if (!validOrder.includes(order)) {
+    return Promise.reject({
+      status: 400,
+      msg: "Invalid order",
+    });
+  }
+
+  let queryString = `SELECT * FROM reviews`;
+
+  if (category) {
+    queryValues.push(category);
+    queryString += ` WHERE category = $${queryValues.length}`;
+  }
+
+  if (sort_by || order) {
+    queryString += ` ORDER BY ${sort_by} ${order}`;
+  }
+
+  return DB.query(queryString, queryValues).then((results) => {
+    if (results.rows.length === 0) {
+      return Promise.reject({ status: 404, msg: "No reviews found" });
+    } else {
+      return results.rows;
+    }
+  });
+};
