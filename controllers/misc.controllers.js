abd8cb60d2abbeb7ef7b6bfe00e8af83f4f803b8
scottkelly36 <scottkelly36@gmail.com>
2022-07-08 12:29:53 +0100

    changed file path

--- controllers/misc.controllers.js
+++ controllers/misc.controllers.js
@@ -1,7 +1,9 @@
 const fs = require("fs/promises");
 
 exports.getEndpoints = (req, res, next) => {
-  return fs.readFile("./endpoints.json", "utf8").then((result) => {
-    res.status(200).send({ endpoints: JSON.parse(result) });
+  return fs.readFile(`${__dirname}/../endpoints.json`, "utf8").then((result) => {
+    res.status(200).send({
+      endpoints: JSON.parse(result)
+    });
   });
 };
