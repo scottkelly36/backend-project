03650468932a1996296a20b8a6e58b8a8fafabc6
scottkelly36 <scottkelly36@gmail.com>
2022-07-08 13:43:18 +0100

    added if statement to connection

--- db/connection.js
+++ db/connection.js
@@ -3,9 +3,14 @@ const {
 } = require('pg');
 const ENV = process.env.NODE_ENV || 'development';
 
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error('PGDATABASE or DATABASE_URL not set');
+}
+
 require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
+
 const config =
   ENV === 'production' ? {
     connectionString: process.env.DATABASE_URL,
@@ -13,6 +18,7 @@ const config =
       rejectUnauthorized: false,
     },
   } : {};
+
 
 if (!process.env.PGDATABASE) {
   throw new Error('PGDATABASE not set');
