461f32eb0ae88e08de5b43697d84341c0cc53304
scottkelly36 <scottkelly36@gmail.com>
2022-07-08 13:17:05 +0100

    prodc changes

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,21 @@
-const { Pool } = require('pg');
+const {
+  Pool
+} = require('pg');
 const ENV = process.env.NODE_ENV || 'development';
 
 require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
+const config =
+  ENV === 'production' ? {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  } : {};
 
 if (!process.env.PGDATABASE) {
   throw new Error('PGDATABASE not set');
 }
 
-module.exports = new Pool();
+module.exports = new Pool(config);
