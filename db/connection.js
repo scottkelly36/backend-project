e419b734e45c448f4d3dbef13c47e9e0d5456e2d
scottkelly36 <scottkelly36@gmail.com>
2022-07-08 14:27:59 +0100

    removed if

--- db/connection.js
+++ db/connection.js
@@ -10,6 +10,14 @@ require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
+
+
+
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error('PGDATABASE or DATABASE_URL not set');
+}
+
+
 const config =
   ENV === 'production' ? {
     connectionString: process.env.DATABASE_URL,
@@ -17,14 +25,4 @@ const config =
       rejectUnauthorized: false,
     },
   } : {};
-
-
-if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-  throw new Error('PGDATABASE or DATABASE_URL not set');
-}
-
-if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
-}
-
 module.exports = new Pool(config);
