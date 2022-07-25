21a76029213ca832df13ca2350d67e426e37ba95
scottkelly36 <scottkelly36@gmail.com>
2022-07-25 09:19:23 +0100

    allow cors

--- app.js
+++ app.js
@@ -1,75 +1,69 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-
+const cors = require('cors');
 
 const {
-  getEndpoints
-} = require("./controllers/misc.controllers");
-
-
+	getEndpoints,
+} = require('./controllers/misc.controllers');
 
 //categories controllers
 const {
-  getCategories
-} = require("./controllers/categories.controllers");
-
-
-
+	getCategories,
+} = require('./controllers/categories.controllers');
 
 //reviews controllers
 const {
-  getReviewById,
-  patchReviewVotes,
-  postReviewComment,
-  getReviewComments,
-  getReviews,
-} = require("./controllers/reviews.controller");
-
-
+	getReviewById,
+	patchReviewVotes,
+	postReviewComment,
+	getReviewComments,
+	getReviews,
+} = require('./controllers/reviews.controller');
 
 //users controllers
 const {
-  getUsers
-} = require("./controllers/users.controllers");
-
-
+	getUsers,
+} = require('./controllers/users.controllers');
 
 //comment controllers
 const {
-  deleteComment
+	deleteComment,
 } = require('./controllers/comments.controllers');
 
 //errors
 const {
-  handleServerErrors,
-  handleWrongEndpoint,
-  handleNotFound,
-  handleCustom,
-} = require("./middleware/errors.middleware");
+	handleServerErrors,
+	handleWrongEndpoint,
+	handleNotFound,
+	handleCustom,
+} = require('./middleware/errors.middleware');
 
 app.use(express.json());
+app.use(cors());
 
-app.get("/api", getEndpoints);
-app.get("/api/categories", getCategories);
+app.get('/api', getEndpoints);
+app.get('/api/categories', getCategories);
 
-app.get("/api/reviews/:review_id", getReviewById);
-app.patch("/api/reviews/:review_id", patchReviewVotes);
-app.post("/api/reviews/:review_id/comments", postReviewComment);
-app.get("/api/reviews/:review_id/comments", getReviewComments);
-app.get("/api/reviews", getReviews);
-
+app.get('/api/reviews/:review_id', getReviewById);
+app.patch('/api/reviews/:review_id', patchReviewVotes);
+app.post(
+	'/api/reviews/:review_id/comments',
+	postReviewComment
+);
+app.get(
+	'/api/reviews/:review_id/comments',
+	getReviewComments
+);
+app.get('/api/reviews', getReviews);
 
 app.get('/api/users', getUsers);
 
 app.delete('/api/comments/:comment_id', deleteComment);
 
-
-
-
-app.use("*", (req, res) => {
-  res.status(404).send({
-    msg: "Sorry we cant find that end point",
-  });
+app.use('*', (req, res) => {
+	res.status(404).send({
+		msg: 'Sorry we cant find that end point',
+	});
 });
 
 app.use(handleNotFound);
